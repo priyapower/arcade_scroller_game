233ae0b5d57dc730bf259e57548637bcf66eb1b7
Priya Power <49959312+priyapower@users.noreply.github.com>
2021-02-26 10:28:20 -0700

    Add score code with better user control and annotations

--- 07_score_better_control_annotations.py
+++ 07_score_better_control_annotations.py
@@ -9,12 +9,11 @@ SCREEN_TITLE = "Priya's 2D Funhouse"
 
 CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
-# NEW CONSTANT FOR COIN SCALING
 COIN_SCALING = 0.5
 
 PLAYER_MOVEMENT_SPEED = 3
 GRAVITY = 0.8
-PLAYER_JUMP_SPEED = 15
+PLAYER_JUMP_SPEED = 13
 
 LEFT_VIEWPORT_MARGIN = 250
 RIGHT_VIEWPORT_MARGIN = 250
@@ -30,7 +29,7 @@ class MyGame(arcade.Window):
     def __init__(self):
 
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-        # New Coin List Variable Setup
+
         self.coin_list = None
         self.wall_list = None
         self.player_list = None
@@ -48,13 +47,13 @@ class MyGame(arcade.Window):
         self.view_bottom = 0
         self.view_left = 0
 
-        # Creates a variable that holds our sound for coin collection
-        # .load_sound: https://arcade.academy/arcade.html?highlight=.load_sound#arcade.load_sound
-            # Loads a sound for use with .play_sound later
+        # SETS INITIAL VALUE OF SCORE TO ZERO AND HELPS US KEEP TRACK OF THE SCORE BY GIVING US ACCESS TO A self.score VARIABLE THAT WE CAN ADD/SUBTRACT POINTS TO/FROM
+        self.score = 0
+
         self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
-        # Creates a variable that holds our sound for player jumping
         self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
 
+        # I LIKE TO CHANGE MY BACKGROUND COLOR FOR MY PERSONAL PREFERENCE OF THIS PRETTY PINK COLOR
         arcade.set_background_color(arcade.csscolor.MEDIUM_VIOLET_RED)
 
     def setup(self):
@@ -63,15 +62,17 @@ class MyGame(arcade.Window):
         self.view_bottom = 0
         self.view_left = 0
 
+        # FOR THE RESTART, THIS SETS SCORE TO 0
+        self.score = 0
+
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
-        # New Coin Sprite List
         self.coin_list = arcade.SpriteList()
 
         image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 128
+        self.player_sprite.center_y = 96
         self.player_list.append(self.player_sprite)
 
         for x in range(0, 1250, 64):
@@ -91,15 +92,11 @@ class MyGame(arcade.Window):
             wall.position = coordinate
             self.wall_list.append(wall)
 
-        # Adds a new sprite: the coin
-        # Use a loop to place some coins for our character to pick up
         for x in range(128, 1250, 256):
             image_source4 = ":resources:images/items/coinGold.png"
             coin = arcade.Sprite(image_source4, COIN_SCALING)
             coin.center_x = x
-            # Makes the coin float a little off the ground
             coin.center_y = 96
-            # Adds coin to coin list
             self.coin_list.append(coin)
 
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
@@ -108,14 +105,28 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         """ Render the screen. """
+
         arcade.start_render()
 
         self.wall_list.draw()
         self.coin_list.draw()
         self.player_list.draw()
 
+        # DRAW THE SCORE ON THE SCREEN
+        # HAS DYNAMIC CAPABILITIES - IT SCROLLS WITH YOUR PLAYER
+        # score_text is a String with the self.score interpolated into the Score statement
+        score_text = f"Score: {self.score}"
+        # draw text is an arcade function that draws text to the screen
+            # https://arcade.academy/arcade.html?highlight=draw_text#arcade.draw_text
+        # The arguments are as follows:
+            # .draw_text(TEXT, X-START, Y-START, COLOR, FONT-SIZE)
+        # So this creates a text box drawn on the bottom left of the screen with the score printed from self.score in the color white at font size 18
+        arcade.draw_text(score_text, 10 + self.view_left, 10 + self.view_bottom,
+                         arcade.csscolor.WHITE, 18)
+
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
+
         # MY USER CONTROL UPDATES
         if key == arcade.key.UP or key == arcade.key.W or key == arcade.key.SPACE:
             self.up_pressed = True
@@ -128,6 +139,7 @@ class MyGame(arcade.Window):
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
+
         # MY USER CONTROL UPDATES
         if key == arcade.key.UP or key == arcade.key.W:
             self.up_pressed = False
@@ -138,16 +150,15 @@ class MyGame(arcade.Window):
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.right_pressed = False
 
-    def update(self, delta_time):
+    def on_update(self, delta_time):
         """ Movement and game logic """
+
         # MY USER CONTROL UPDATES
         self.player_sprite.change_x = 0
 
         if self.up_pressed and not self.down_pressed:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = PLAYER_JUMP_SPEED
-                # NEW INFO FROM SOUNDS
-                # This plays the sound from jump_sound which was set in __init__
                 arcade.play_sound(self.jump_sound)
         elif self.down_pressed and not self.up_pressed:
             self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
@@ -157,22 +168,17 @@ class MyGame(arcade.Window):
             self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
         self.player_list.update()
+
         self.physics_engine.update()
 
-        # The code for what happens when a player collides with a coin
-        # Checks the coin sprite list for collisions AND saves to a new variable "coin_hit_list"
-        # .check_for_collision_with_list: https://arcade.academy/arcade.html?highlight=check_for_collision_with_list#arcade.check_for_collision_with_list
-            # Check for a collision between a sprite, and a list of sprites
-            # 2 Parameters: (sprite_to_check, sprite_to_check_AGAINST)
         coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
                                                              self.coin_list)
 
-        # For every coin sprite in the coin HIT list
         for coin in coin_hit_list:
-            # Remove the coin from any sprite list it belongs to
             coin.remove_from_sprite_lists()
-            # Play the sound set in __init__
             arcade.play_sound(self.collect_coin_sound)
+            # EVERY TIME A PLAYER HITS A COIN, THIS INCREMENTS BY 1 FOR SCORE COLLECTION
+            self.score += 1
 
         # --- Manage Scrolling ---
         changed = False
@@ -206,11 +212,13 @@ class MyGame(arcade.Window):
                                 self.view_bottom,
                                 SCREEN_HEIGHT + self.view_bottom)
 
+
 def main():
     """ Main method """
     window = MyGame()
     window.setup()
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
